f28b982f0514bd193966b02733d0273138cf9e37
gaellebega <gaellebega@gmail.com>
2025-11-26 19:27:51 +0200

    fibbonacci number

--- EASY/sumoftwo.js
+++ EASY/sumoftwo.js
@@ -33,3 +33,11 @@ const response=thisApalindrome("MOM");
 console.log(response);
 
 //fibonnacci series to get the next number you have to add up the 2 previous numbers 
+//f(n)=f(n-1)+f(n-2); for n>1;
+var fib =function(n){
+  const arr = [0,1];
+  for(let i=2;i<=n;i++){
+    arr.push(arr[i-1]+arr[i-2]);
+  }
+  return n;
+}
