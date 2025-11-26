cffc067824a02629d9b4d604185248224d8677c0
gaellebega <gaellebega@gmail.com>
2025-11-26 19:03:46 +0200

    palindrome of numbers

--- EASY/sumoftwo.js
+++ EASY/sumoftwo.js
@@ -11,3 +11,25 @@ var twosum=function(nums,target){
   }
 }
 console.log(twosum([2,7,11,15],9));
+
+
+//given an integer and tellif it is a palindrome or not
+//palindrome number is a number which reads the same when you read backwards or frontwards
+
+const isPalindrome=function(x){
+  //reverse the array in the place like just the way it is
+  //121  => "121" =>["1","2","1"]  =>  ["1","2","1"]
+
+  return x < 0 ? false:x=== +x.toString().split("").reverse().join();
+}
+const res =isPalindrome(19);
+console.log(res);
+
+
+const thisApalindrome=function(str){
+return str === str.split("").reverse().join("");
+}
+const response=thisApalindrome("MOM");
+console.log(response);
+
+//fibonnacci series to get the next number you have to add up the 2 previous numbers 
