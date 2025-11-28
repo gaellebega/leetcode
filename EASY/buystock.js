5b1883a7a835576c4f81b456228a0ce7f5a40e30
gaellebega <gaellebega@gmail.com>
2025-11-28 23:19:49 +0200

    new fike created

--- EASY/buystock.js
+++ EASY/buystock.js
@@ -6,8 +6,10 @@ for(let i=0;i<prices.length;i++){
   for(let i=i+1;i<prices.length;j++){
     const currentProfit = prices[j] - prices[i];
     //this means if the current profit is greater then the global profit then the global profit is equal to the currentProfit
+    //the grobal profit is the maximum profit we can have
     if(currentProfit >grobalProfit) grobalProfit =currentProfit;
   }
 }
 return grobalProfit;
 }
+//this above function is about buying the stock
